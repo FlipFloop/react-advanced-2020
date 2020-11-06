27ecb36560b3c7b8d55118c46eb19649cdb4c910
Victor GUYARD <19635051+FlipFloop@users.noreply.github.com>
2020-11-06 16:11:27 -0500

    stuff

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,26 +2,37 @@ import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
+import { reducer } from "./reducer";
+const defaultState = {
+  people: data,
+  isModalOpen: false,
+  modalContent: "hello world",
+};
 
 const Index = () => {
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
   const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
       setName("");
     } else {
-      setShowModal(true);
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
 
   return (
     <>
-      {showModal && <Modal />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -36,10 +47,17 @@ const Index = () => {
           </button>
         </div>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div className="item" key={person.id}>
-            {person.name}
+            <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              Close Modal
+            </button>
           </div>
         );
       })}
